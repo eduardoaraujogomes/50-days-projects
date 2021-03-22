78960a758799190151cb9d34c4cd0361eac15cd1
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2021-03-22 11:19:01 -0300

    Day 8 Project - Form Input Wave

---
+++ 8DAY/script.js
@@ -0,0 +1,6 @@
+const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText.split('').map((letter, index) =>
+        `<span style="transition-delay:${index * 50}ms">${letter}</span>`).join('')
+})
