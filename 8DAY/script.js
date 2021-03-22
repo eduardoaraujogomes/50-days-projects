f2cfa40bec26bce1acc810992a354c8ab96379d8
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2021-03-22 12:04:39 -0300

    Day 8 Project - Input Wave

--- 8DAY/script.js
+++ 8DAY/script.js
@@ -4,3 +4,5 @@ labels.forEach(label => {
     label.innerHTML = label.innerText.split('').map((letter, index) =>
         `<span style="transition-delay:${index * 50}ms">${letter}</span>`).join('')
 })
+
+
